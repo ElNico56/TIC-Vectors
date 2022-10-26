2f264ae04b3418f6689461754623042901d9a0e6
ElNico56 <meeqnico@gmail.com>
2022-10-26 16:28:26 -0300

    prettified again

--- vectors.lua
+++ vectors.lua
@@ -1,81 +1,44 @@
-local vectmt={}
+local vectmt = {}
 vectmt.__index = vectmt
 
-local function _vect_(X, Y, Z)
-	return setmetatable({
-		x=tonumber(X) or 0,
-		y=tonumber(Y) or 0,
-		z=tonumber(Z) or 0,
-	},vectmt)
+local function vect(x, y, z)
+    return setmetatable({
+        x = tonumber(x) or 0,
+        y = tonumber(y) or 0,
+        z = tonumber(z) or 0
+    }, vectmt)
 end
 
-vectmt.__add=function(s, o)
-	return _vect_(
-		s.x+o.x,
-		s.y+o.y,
-		s.z+o.z
-	)
+vectmt.__add = function(s, o) return vect(s.x + o.x, s.y + o.y, s.z + o.z) end
+
+vectmt.__sub = function(s, o) return vect(s.x - o.x, s.y - o.y, s.z - o.z) end
+
+vectmt.__mul = function(s, n) return vect(s.x * n, s.y * n, s.z * n) end
+
+vectmt.__div = function(s, n) return vect(s.x / n, s.y / n, s.z / n) end
+
+vectmt.__unm = function(s) return vect(-s.x, -s.y, -s.z) end
+
+vectmt.__tostring = function(s)
+    return '(' .. s.x .. ', ' .. s.y .. ', ' .. s.z .. ')'
 end
 
-vectmt.__sub=function(s, o)
-	return _vect_(
-		s.x-o.x,
-		s.y-o.y,
-		s.z-o.z
-	)
+vectmt.dot = function(s, o) return s.x * o.x + s.y * o.y + s.z * o.z end
+
+vectmt.cross = function(s, o)
+    return vect(s.y * o.z - s.z * o.y, s.z * o.x - s.x * o.z,
+                s.x * o.y - s.y * o.x)
 end
 
-vectmt.__mul=function(s, n)
-	return _vect_(
-		s.x*n,
-		s.y*n,
-		s.z*n
-	)
+vectmt.len = function(s) return math.sqrt(s.x * s.x + s.y * s.y + s.z * s.z) end
+
+vectmt.norm = function(s) return s:__div(s:len()) end
+
+vectmt.round = function(s, t)
+    t = t or 1
+    return vect(math.floor((s.x + t * 0.5) / t) * t,
+                math.floor((s.y + t * 0.5) / t) * t,
+                math.floor((s.z + t * 0.5) / t) * t)
 end
 
-vectmt.__div=function(s, n)
-	return _vect_(
-		s.x/n,
-		s.y/n,
-		s.z/n
-	)
-end
-
-vectmt.__unm=function(s)
-	return _vect_(-s.x,-s.y,-s.z)
-end
-
-vectmt.dot=function(s, o)
-	return s.x*o.x+s.y*o.y+s.z*o.z
-end
-
-vectmt.cross=function(s, o)
-	return _vect_(
-		s.y*o.z-s.z*o.y,
-		s.z*o.x-s.x*o.z,
-		s.x*o.y-s.y*o.x
-	)
-end
-
-vectmt.len=function(s)
-	return math.sqrt(s.x^2+s.y^2+s.z^2)
-end
-
-vectmt.norm=function(s)
-	return s:__mul(1/s:len())
-end
-
-vectmt.round=function(s, t)
-	t=t or 1
-	return _vect_(
-		math.floor((s.x+t*0.5)/t)*t,
-		math.floor((s.y+t*0.5)/t)*t,
-		math.floor((s.z+t*0.5)/t)*t
-	)
-end
-
-vectmt.__tostring=function(s)
-	return '('..s.x..', '..s.y..', '..s.z..')'
-end
-
-return _vect_
+return vect
